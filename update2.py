9900b25ac70acf86b7e1c0be3103fcb63e23d41c
OkayMing <cmy6373855@163.com>
2023-05-17 16:39:21 +0800

    尝试vbs实现

---
+++ update2.py
@@ -0,0 +1,102 @@
+# coding=UTF-8
+import os
+import sys
+import socket
+import paramiko
+import subprocess
+import configparser
+
+
+# 用IP的开头判断是否出差在外
+def is_company_network():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        address = ("8.8.8.8", 80)
+        s.connect(address)
+        sockname = s.getsockname()
+        ip = sockname[0]
+        port = sockname[1]
+    finally:
+        s.close()
+        
+    if ip.startswith('10.12'):
+        return True
+    
+    return False
+
+
+#编写bat脚本，删除旧程序，运行新程序
+def writeUpgrade(exename):
+    #b = open("upgrade.bat",'w')
+    b = open("upgrade.vbs",'w')
+    #print(oldversion)
+    #print(newversion)
+    #TempList = "@echo off\n"
+    '''TempList = "ssh cmy@10.12.192.62\n"  #判断是否有新版本的程序，没有就退出更新。
+    TempList += "timeout /t 2 /nobreak\n"  #等待2秒
+    TempList += "cmy\n"
+    TempList += "timeout /t 10 /nobreak\n"  #等待10秒
+    TempList += "del " + os.path.realpath(exe_name) + "\n" #删除旧程序
+    TempList += "copy  z:\\" + exe_name + " " + exe_name + '\n' #复制新版本程序
+    TempList += "echo 更新完成，正在启动...\n"
+    TempList += "timeout /t 3 /nobreak\n"
+    TempList += "start " + exe_name + '\n'   #"start 1.bat\n"
+    TempList += "exit"'''
+    '''TempList = "# coding=UTF-8\n\n"
+    TempList += "import os\n"
+    TempList += "import time\n"
+    TempList += "import subprocess\n"
+    TempList += "import paramiko\n\n"
+    TempList += "os.remove(os.getcwd()+'\\"+exename+"')\n"
+    TempList += "ssh = paramiko.Transport(('10.12.192.62',22))\n"
+    TempList += "ssh.connect(username='cmy', password='cmy')\n"
+    TempList += "stfp = paramiko.SFTPClient.from_transport(ssh)\n"
+    TempList += "stfp.get('/home/cmy/atc/Personal_Folder/CMY/WPAutotest/"+exename+"',r'"+ os.getcwd()+"\\"+exename+"')\n"
+    TempList += "stfp.close()\n"
+    TempList += "ssh.close()\n"
+    TempList += "time.sleep(2)\n"
+    TempList += "os.system('start " + exename +"')"'''
+    TempList = 'Dim WshShell \n\n'
+    TempList += 'Set WshShell=WScript.CreateObject("WScript.Shell")\n'
+    TempList += 'WshShell.Run "cmd.exe"\n'
+    TempList += 'WScript.Sleep 1500\n'
+    TempList += 'WshShell.SendKeys "ssh cmy@10.12.192.62"\n'
+    TempList += 'WshShell.SendKeys "{ENTER}"\n'
+    TempList += 'WScript.Sleep 1500\n'
+    TempList += 'WshShell.SendKeys "cmy"\n'
+    TempList += 'WshShell.SendKeys "{ENTER}"\n'
+    TempList += 'WshShell.SendKeys "{ENTER}"\n'
+    TempList += 'WshShell.SendKeys "{ENTER}"\n'
+    b.write(TempList)
+    b.close()
+    subprocess.Popen("python upgrade.py") #不显示cmd窗口
+    #os.system('python upgrade.py')  #显示cmd窗口
+    #os.remove("upgrade.py")
+    #os.system('start Autotest.exe')
+    sys.exit()
+    #如果是FTP就编写FTP的相应代码
+
+def checkVersion(): #检查版本
+    if not is_company_network():
+        print('连接公司内网后进行更新')
+    if not os.path.isdir('z:\\'):  #判断是否有z盘，没有就mount，使用mount命令需要提前安装好NFS客户端
+        server_path = f'\\\\10.12.192.62\home\cmy\\atc\Personal_Folder\CMY\WPAutotest'
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+        ssh.connect(hostname ='10.12.192.62' , port=22, username='cmy', password='cmy')
+        stdin, stdout, stderr = ssh.exec_command('cat ~/atc/Personal_Folder/CMY/WPAutotest/version.ini')
+        newversion = stdout.read().decode()[18:]
+        #print(newversion)
+        #config = configparser.ConfigParser()  # 类实例化
+        #config.read(versioninfo)
+        #value = config['select']['version'] #获取版本号
+        #print(value)
+    #path = f'{server_path}\\version.ini'
+    #print(path)
+    #config.read(path)
+    #value = config['select']['version'] #获取版本号
+    #print(value)
+    return newversion
+    #也可以使用以下代码获取程序版本号
+    #url = r'http://172.18.114.172/index.html'
+    #value = requests.get(url).text
